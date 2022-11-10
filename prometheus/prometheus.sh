dcd1df8b21075fcc76782838a6f4c7b476fd1842
_ <diegogslomp@gmail.com>
2022-11-09 23:49:53 -0300

    fix yml path

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -2,5 +2,5 @@
 
 docker run \
     -p 9090:9090 \
-    -v ~/repos/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v ~/repos/stacks/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
     prom/prometheus
