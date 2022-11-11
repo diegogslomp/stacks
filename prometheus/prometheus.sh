5769e6d417d3dce787674df52647f90dd24f47f6
Diego Gobbi Slomp <diegogslomp@gmail.com>
2022-11-11 09:58:40 -0300

    add node_exporters to prometheus

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -d \
-    -p 9090:9090 \
+docker run --network=host --detach \
     -v ~/repos/stacks/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v ~/repos/stacks/prometheus/prometheus.rules.yml:/etc/prometheus/prometheus.rules.yml \
+    -v prometheus_data:/prometheus \
     --name prometheus prom/prometheus
